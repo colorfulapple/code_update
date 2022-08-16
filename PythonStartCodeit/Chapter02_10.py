22c3dcca045bf8e8183fe1a4445160ad2e3099aa
colorfulapple <107483721+colorfulapple@users.noreply.github.com>
2022-08-16 16:17:13 +0900

    Create Chapter02_10.py

---
+++ PythonStartCodeit/Chapter02_10.py
@@ -0,0 +1,8 @@
+def multiply_three_numbers(num1, num2, num3):
+    print(num1 * num2 * num3)
+
+
+# 테스트 코드
+multiply_three_numbers(7, 3, 5)
+multiply_three_numbers(21, 4, 9)
+multiply_three_numbers(-7, 6, 3)
