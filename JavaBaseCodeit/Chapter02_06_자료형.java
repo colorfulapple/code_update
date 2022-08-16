0c75935d7e5e1c6389222938ea4e679c8e509d9f
colorfulapple <107483721+colorfulapple@users.noreply.github.com>
2022-08-17 00:18:08 +0900

    Create Chapter02_06_자료형.java

---
+++ JavaBaseCodeit/Chapter02_06_자료형.java
@@ -0,0 +1,21 @@
+public class JavaBaseCodeit_Chapter03_03 {
+    public static void main(String[] args) {
+        boolean myBoolen = true;
+        myBoolen = false;           // 논리
+
+        int myInt = 1;              // 정수
+
+        long myLong = 12345678910L; // 정수 (int로 표현되지 않는 경우, 맨 뒤 L 붙임)
+
+        double myDouble = 3.14;     // 실수
+
+        String myString = "Hello!"; // 문자
+
+        String myOldIs = "Hello, I'm ";
+        String dot = ".";
+
+        System.out.print(myOldIs);
+        System.out.print(22);
+        System.out.println(dot);
+    }
+}
