16447bf1cdd8eed2bf9935bb6b42c5561fc7b8a4
colorfulapple <107483721+colorfulapple@users.noreply.github.com>
2022-08-31 15:49:02 +0900

    Update Chapter03_10_average.java

--- JavaBaseCodeit/Chapter03_10_average.java
+++ JavaBaseCodeit/Chapter03_10_average.java
@@ -1,3 +1,5 @@
+public class JavaBaseCodeit_Chapter03_10_average {
+    public static void main(String[] args) {
         int koreanScore = 86;
         int mathScore = 94;
         int scienceScore = 87;
@@ -5,3 +7,5 @@
 
         double average = (double) (koreanScore + mathScore + scienceScore + computerScore)/4;
         System.out.println(average);
+    }
+}
